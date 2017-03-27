8f865cec7c219b44632987840f5d8ab256cbda2f
Vibin Nair <vibin.nair@morningstar.com>
2017-03-27 17:59:41 +0530

    Chapter_4: Property Observer and NumberFormatter added.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -12,24 +12,52 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    var farhenheitValue: Measurement<UnitTemperature>?
+    //
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    // Initializing a property using a closure;
+    var numberFormatter: NumberFormatter = {
+        let nf  = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
     
     var celsiusValue: Measurement<UnitTemperature>? {
-        if let fahrenheitValue = farhenheitValue {
+        if let fahrenheitValue = fahrenheitValue {
             return fahrenheitValue.converted(to: .celsius)
         }
         return nil
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.updateCelsiusLabel()
+    }
+    
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        if let text = textField.text, !text.isEmpty {
-            self.celsiusLabel.text = textField.text
+        if let text = textField.text, let value = Double(text) {
+            self.fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         } else {
-            self.celsiusLabel.text = "???"
+            self.fahrenheitValue = nil
         }
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         self.textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = self.celsiusValue {
+            self.celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            self.celsiusLabel.text = "???"
+        }
     }
 }
