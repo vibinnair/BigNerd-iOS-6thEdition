089bb501ee9ef82c25f79bfd566f675a8be9cd60
Vibin Nair <vibin.nair@morningstar.com>
2017-04-10 12:40:52 +0530

    Chapter_5: View Controllers: Added MapViewController .

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Vibin Nair on 07/04/17.
+//  Copyright © 2017 VibinBigNerd. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
